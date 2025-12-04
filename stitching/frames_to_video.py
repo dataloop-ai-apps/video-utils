da00dd7d51dff89aa7ed61b17645d7f956729206
RoniDataloopAi <roni.a@dataloop.ai>
2025-12-04 18:13:10 +0200

    changed to sort by name

--- stitching/frames_to_video.py
+++ stitching/frames_to_video.py
@@ -68,10 +68,10 @@ class ServiceRunner(dl.BaseServiceRunner):
             logger.error("No images found in specified directory")
             return []
 
-        def sort_by_frame_index(item: dl.Item) -> tuple:
-            return (item.metadata["splitting_sub_videos_index"], item.metadata["splliting_frame_index"])
+        # def sort_by_frame_index(item: dl.Item) -> tuple:
+            # return (item.metadata["splitting_sub_videos_index"], item.metadata["splliting_frame_index"])
 
-        return sorted(items, key=sort_by_frame_index, reverse=False)
+        return sorted(items, key=lambda x: x.name)
 
     def stitch_and_upload(self, cv_frames: List[np.ndarray], local_output_folder: str) -> dl.Item:
         """
