55e37998680430ef400fdca9b6869037702c53ea
husamm456 <husam.m@dataloop.ai>
2026-07-02 14:31:47 +0300

    Preserve origin_video_name metadata in frames_to_video and videos_to_video output items (#20)

--- stitching/videos_to_video.py
+++ stitching/videos_to_video.py
@@ -292,6 +292,9 @@ class ServiceRunner(dl.BaseServiceRunner):
             local_path=output_video_path, remote_path="/" + self.dl_output_dir.lstrip('/')
         )
         video_item.fps = fps
+        origin_video_name = item.metadata.get('origin_video_name', None)
+        if origin_video_name is not None:
+            video_item.metadata['origin_video_name'] = origin_video_name
         video_item.update()
         logger.info("uploading annotations to video")
         self.upload_annotations(video_item, merged_video_annotations, merged_video_frames)
